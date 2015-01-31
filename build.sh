385000ae50403b5fb92b6cb972b69e186aa49836
Caleb Land <caleb@land.fm>
2015-01-31 00:27:32 -0500

    php->mysql

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Building mysql:5.7"
-docker build -t docker.rodeopartners.com/php:5.7 5.7
+docker build -t docker.rodeopartners.com/mysql:5.7 5.7
+
+echo "Building mysql:5.7-toolbox"
+docker build -t docker.rodeopartners.com/mysql:5.7-toolbox 5.7-toolbox
